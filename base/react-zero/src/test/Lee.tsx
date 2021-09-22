742d9eedca5a9b8bad7b413475d6b2726cdece32
柳振江 <906035221@qq.com>
2021-09-22 23:50:15 +0800

    添加vue源码

--- base/react-zero/src/test/Lee.tsx
+++ base/react-zero/src/test/Lee.tsx
@@ -5,13 +5,19 @@ interface IProps {
   name: string;
 }
 
+interface IState {
+  width?: number,
+  count?: number;
+}
+
 /**
  * component 第一个参数，定义props类型
  */
-class Lee extends Component<IProps, any> {
+class Lee extends Component<IProps, IState> {
 
   state = {
-    count: 0
+    count: 0,
+    width: 200,
   }
 
   componentDidMount() {
@@ -22,8 +28,11 @@ class Lee extends Component<IProps, any> {
   }
 
   clickHandler = (event: any):void => {
-    console.log(event);
     this.setState({count: this.state.count + 1})
+  }
+
+  change = () => {
+    this.setState({ width: this.state.width + 100 })
   }
 
   render(): ReactNode {
@@ -31,6 +40,14 @@ class Lee extends Component<IProps, any> {
       <div>
         <button onClick={this.clickHandler}>+1</button>
         <span className={lee.red}>{ this.state.count }</span>
+
+        <div style={{
+          width: `${this.state.width}px`,
+          height: '200px',
+          backgroundColor: "dimgray"
+        }}>
+          <button onClick={this.change}>change</button>
+        </div>
       </div>
     );
   }
